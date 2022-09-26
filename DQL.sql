74ab14fdd75e4fac5d63e705c668b5ccd23324c7
FernandaDias24 <diasfernanda2412@gmail.com>
2022-09-25 22:19:52 -0300

    Respondendo as questões do arquivo DQL

--- DQL.sql
+++ DQL.sql
@@ -1,22 +1,57 @@
 #Responde Perguntos:
 
 #Quais são os 3 produtos mais caros?
-
+SELECT 
+    *
+FROM
+    Produto
+ORDER BY valor_produto DESC
+LIMIT 3;
 
 #Quantas avaliações cada cliente realizou?
-
+SELECT 
+    CPF, COUNT(CPF) AS avaliacao
+FROM
+    avaliacao
+GROUP BY CPF;
 
 #Qual a média das avaliações de cada produto?
-
+SELECT 
+    AVG(avaliação)
+FROM
+    avaliacao;
 
 #Qual a data de avaliação mais recente de cada produto?
-
+SELECT 
+    id_produto, MAX(Av_data)
+FROM
+    avaliacao
+GROUP BY id_produto
+order by id_produto;
 
 #Quais as clientes cujo o nome se inicia com Joana?
-
+SELECT 
+    nome
+FROM
+    Clientes
+WHERE
+    nome LIKE '%Joana%';
 
 #Qual o produto que recebeu a menor avaliação?
-
+SELECT 
+    avaliacao.id_avaliacao, MIN(avaliação)
+FROM
+    avaliacao
+GROUP BY id_avaliacao
+ORDER BY avaliação
+LIMIT 5;
 
 #Quais são os produtos cuja a avaliação mais recente é anterior à 30 dias, liste-os como “Avaliação desatualizada”?
-
+SELECT 
+    MAX(Av_data) as av_desatualizado, id_produto
+    from 
+    avaliacao
+WHERE
+    Av_data < NOW() - INTERVAL 30 DAY
+    group by avaliacao.Av_data
+    order by av_desatualizado desc;
